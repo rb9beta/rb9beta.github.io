da7e4a014baf8d74bf455ce5a109fb8500ed3bc5
rb9beta <119169453+rb9beta@users.noreply.github.com>
2023-08-19 13:57:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (let i = 0; i < query.length; i++) {
 			var element
 			element = document.getElementById("key")
 			if (element) {
-				element.outerHTML = "<p><marquee width=743>[Created 27-11-2022 13:47 BST | This is a beta for the sake of testing " + '"alpha"' + " stuff before committing it to the main page. Yes, the font is different.</marquee></p><h2><b>Beta Links :</b></h2><table border='1'><td class=" + '"box2d"' + "><a href='t592sc'>Trimps+ v5.9.2 (doesn't exist yet)</a></td></table><h2><b>Congrats to :</b></h2><p>[no-one]; if you see this, then you were the first one here! (Or second if you count me.)</p><h2>For making it here!</h2><p>[Last Updated 27-11-2022]</p><p style=" + 'text-align:right' + ">There are <u>no</u> keys on this page. Trust me!</p>"
+				element.outerHTML = "<p><marquee width=743>[Created 27-11-2022 13:47 BST | This is a beta for the sake of testing " + '"alpha"' + " stuff before committing it to the main page. Yes, the font is different.</marquee></p><h2>Beta Links :</h2><table border='1'><td class=" + '"box2d"' + "><a href='t592sc'>Trimps+ v5.9.2 (doesn't exist yet)</a></td></table><h2>Congrats to :</h2><p>[no-one]; if you see this, then you were the first one here! (Or second if you count me.)</p><h2>For making it here!</h2><p>[Last Updated 27-11-2022]</p><p style=" + 'text-align:right' + ">There are <u>no</u> keys on this page. Trust me!</p>"
 			}
 		})
 	}
